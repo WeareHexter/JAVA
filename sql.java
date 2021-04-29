db69fe30fa7a6854beafcd1ffe2ec1ffaf4bc6f0
WeareHexter <83334203+WeareHexter@users.noreply.github.com>
2021-04-29 11:23:25 +0530

    Add files via upload

---
+++ sql.java
@@ -0,0 +1,90 @@
+
+	create table rate
+	(
+	Rattings number(2)
+	);
+
+
+	INSERT INTO  rate(rate) values( 7 );
+	
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+	 Create table comt
+     (
+     Comments Varchar2(5)
+     );
+	
+	insert into comt values ('good');
+
+	line 750
+	select comments , rattings from comt , rate ;
+////////////////////////////////////////////////////////////////////////////////////////////////
+	Create table food_items
+	(
+	ITEM_NO                  number(3),
+	NAME                varchar2(25),
+	CALORIES	        number(6,2),
+	PROTEIN             number(6,2),
+	CARBOHYDRATES	    number(6,2),
+	FATS                number(6,2)	
+	);
+	
+	
+	DATA BASE ;
+	
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(1,'Eggs',80,12,0,11 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(2,'Sambhar',150,9,77,11 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(3,'Chapati',85,9.4,48.8,7.5 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(4,'Butter paneer',147,8.5,10.7,8.1 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(5,'Rice',120,2,30,5 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(6,'Paratha',180,11,42,9 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(7,'Dosa',250,4,30,6 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(8,'Gulabjamun',300,5,27,7 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(9,'Curd',90,3.5,4.7,3 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(10,'Mix veg',298,4,17.9,19.6 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(11,'Banana',105,1,23,0 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(12,'Milk',135,5,4.3,6.5 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(13,'Apple',70,0.3,14,0 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(14,'Bread',82,4,13.8,1.1 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(15,'Popcorn',90,3,18,1 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(16,'Moong Dal',347,31.3,12.8,3.7 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(17,'Arhar dal',343,8,2.8,1.2 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(18,'Lassi',150,3.2,5.7,6.5 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(19,'Pav bhaji',400,9.4,57.9,12 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(20,'Momos',120,0.6,4.1,1.8 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(21,'Vada pav',300,6.6,33.9,4 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(22,'Idli',100,2,10,1 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(22,'Uttapam',170,3,24,7 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(23,'Burger',280,13.3,30.3,10.1 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(24,'French fries',228,2,18.5,13.1 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(25,'Spring roll',154,2.6,16,7.9 );  
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(26,'Noodles',138,11,37,6 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(27,'Fish',100,20,8,10 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(28,'Chicken Tikka',273,22mg,7,20 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(29,'Butter chicken',485,24,8,29 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(30,'Sprouts',30,3.2,6.2,0.2 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(31,'Chole',74,4.3,3.3,4.1 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(32,'Rajma',153,5,11,3 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(33,'Ladoo',250,4.2,24,11 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(34,'Barfi',100,6,16.7,0 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(35,'Pizza',266,12.2,35.7,10.4 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(36,'Pasta',131,7.5,37,0.8 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(37,'Pani puri',125,5.4,29.6,5.2 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(38,'Coca cola',150,0,27,0 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(39,'Biscuits',260,4.8,41.5,8.5 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(40,'Palak paneer',335,10.6,14.1,24 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(41,'Nutri',156,16.6,9.9,9 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(42,'Salad',285,16.9,46.6,9.3 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(43,'Tea',45,2.5,12.2,3.7 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(44,'Coffee',148.2,6.6,15.6,6.7 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(45,'Ice Cream',170,4,25,7 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(46,'Cheese sandwich',497,18,39,19 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(47,'Kurkure',76,1.3,10.7,3.1 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(48,'Lays',160,1,20,14 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(49,'Patties',129,1,14,8 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(50,'Juice',122,2,25.5,0 );
+insert into food_items (ITEM_NO,NAME,CALORIES,PROTEIN,CARBOHYDRATES,FATS)values(51,'Chocolate',530,7.5,56.5,30.5 );
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ 
+     select  sum(CALORIES),sum(PROTEIN),sum(CARBOHYDRATES),sum(FATS)  from Food_items Where name in ('Tea','Coffee','Salad') ;
